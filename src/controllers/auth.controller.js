5d972607d97a4e0da75e2f3052a7552e576f3bcd
dinhtho2001 <phamdinhtho.0105@gmail.com>
2023-03-03 19:28:28 +0700

    update api auth

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,28 +3,41 @@ import User from "../models/user.model.js"
 
 class AuthController {
    async login(req, res) {
-      if (req.body.username != "" && req.body.password != "") {
+      if (!req.body) {
+         return res.status(400).send({
+            message: "UserName or Password can not be empty!"
+        });
+      }
+      try {
          let user = await User.findOne({ where: { username: req.body.username } });       
          if (user) {         
             let password_valid = await bcrypt.compareSync(req.body.password, user.password);
             if (password_valid) {
                return res.status(200).json({ data: user });
             }
-            return res.status(400).json({ error: "sai mat khau" });
+            return res.status(400).send({ message: "Wrong password" });
          }
          else {
-            return res.status(400).json({ error: "sai tai khoan" });
+            return res.status(400).send({ message: "Wrong username" });
          }
+      } catch (error) {
+         res.status(500).send({
+            message: "Error login", error: error
+        });
       }
-      return res.status(400).json({ error: "không được để trống" });
    }
 
    async register(req, res) {
-      if (req.body.username != "" && req.body.password != "") {
-         const userOld = await User.findOne({ where: { username: req.body.username } });
+      if (!req.body) {
+         return res.status(400).send({
+            message: "UserName or Password can not be empty!"
+        });
+      }
+      try {
+         let userOld = await User.findOne({ where: { username: req.body.username } });
          let salt = await bcrypt.genSaltSync(10);
          if (userOld !== null) {
-            return res.status(400).json({ error: "tài khoản đã tồn tại" });
+            return res.status(400).send({ message: "Account already exists" });
          } else {
             var user = {
                username: req.body.username,
@@ -37,9 +50,12 @@ class AuthController {
             const created_user = await User.create(user);
             return res.status(201).json(created_user);
          }
-      } else {
-         return res.status(400).json({ error: "không được để trống" });
+      } catch (error) {
+         res.status(500).send({
+            message: "Error register", error: error
+        });
       }
+     
    }
 }
 
